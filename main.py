93a5e4a3ca96ef2eac1d9e4f2631e3b5fb1f7eb1
Kcrong <iam@devkcr.org>
2022-10-23 21:58:27 +0900

    Update

--- main.py
+++ main.py
@@ -34,6 +34,8 @@ def init_driver():
     options = webdriver.ChromeOptions()
     options.add_argument("headless")
     options.add_argument("window-size=1920x1080")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     options.add_argument("disable-gpu")
     options.add_argument(
         f"user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) "
