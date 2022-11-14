a61125f02e577a56c5081eac6890c1579250c4ae
Kcrong <iam@devkcr.org>
2022-11-14 14:12:26 +0900

    Add vendor

--- cmd/main.go
+++ cmd/main.go
@@ -84,7 +84,7 @@ func main() {
 	}
 
 	go func() {
-		for range time.Tick(time.Hour) {
+		for range time.Tick(time.Hour * 24) {
 			wd, closeFunc, err := driver.Init(c.SeleniumWebDriverHost, c.ShouldRunHeadless, opt)
 			if err != nil {
 				log.Fatalf("%+v", err)
